762b7760fa3f774589b98eea121e14fb84cb5a05
Jean-Baptiste Lemée <jbl@buildwall.com>
2011-04-24 21:30:32 +0200

    Use redis instead of EHCache for better persistance :P

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.pool.impl.GenericObjectPool.Config;
 import play.*;
 import play.cache.Cache;
 import play.libs.Time;
@@ -9,22 +10,33 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 public class Application extends Controller {
 
+    static final JedisPool pool = new JedisPool(new Config(),
+            Play.configuration.getProperty("redis.host", "localhost"),
+            Integer.valueOf(Play.configuration.getProperty("redis.port", "6379")),
+            10000,
+            Play.configuration.getProperty("redis.password", "foobared"));
+
     public static void index() {
+        Cache.get("");
         render();
     }
 
     public static void getUrl(String key) {
-        String redirectUrl = (String) Cache.get("url#" + key);
+        Jedis jedis = pool.getResource();
+        String redirectUrl = jedis.get("url#" + key);
         if (redirectUrl == null) {
             notFound();
         }
-        redirect(redirectUrl, true);
+        redirect(redirectUrl);
     }
 
     public static String postUrl(String url) {
+        Jedis jedis = pool.getResource();
         String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
         int size = 1;
         String key = null, exitingUrl = null;
@@ -34,12 +46,14 @@ public class Application extends Controller {
             size++;
         } while (exitingUrl != null);
 
-        Cache.safeAdd("url#" + key, url, null);
+        String niceUrl = url.startsWith("http://")?url:"http://"+url;
+        jedis.set("url#" + key, niceUrl);
         return key;
     }
 
     private static String findUrl(String key) {
-        return (String) Cache.get("url#" + key);
+        Jedis jedis = pool.getResource();
+        return jedis.get("url#" + key);
     }
 
 }
