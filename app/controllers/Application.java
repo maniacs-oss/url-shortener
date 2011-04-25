d1b89cdec8a1edb22d539ac1e0550cda3a7376c4
Jean-Baptiste Lemée <jbl@buildwall.com>
2011-04-25 10:30:36 +0200

    Jedis Pool doesnt work anymore after a some time

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,73 +1,53 @@
 package controllers;
 
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.pool.impl.GenericObjectPool.Config;
 import play.*;
 import play.mvc.*;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisShardInfo;
 
 public class Application extends Controller {
 
-    static final JedisPool pool = new JedisPool(new Config(),
+    static final JedisShardInfo redisConfig = new JedisShardInfo(
             Play.configuration.getProperty("redis.host", "localhost"),
-            Integer.valueOf(Play.configuration.getProperty("redis.port", "6379")),
-            300,
-            Play.configuration.getProperty("redis.password", "foobared"));
+            Integer.valueOf(Play.configuration.getProperty("redis.port", "6379")));
+
+    static {
+        redisConfig.setPassword(Play.configuration.getProperty("redis.password", "foobared"));
+    }
 
     public static void index() {
         render();
     }
 
     public static void getUrl(String key) {
-        Jedis jedis = null;
-        try {
-            jedis = pool.getResource();
-            String redirectUrl = jedis.get("url#" + key);
-            if (redirectUrl == null) {
-                notFound();
-            }
-            redirect(redirectUrl);
-        } finally {
-            if (jedis != null) {
-                pool.returnResource(jedis);
-            }
+        Jedis jedis = new Jedis(redisConfig);
+        String redirectUrl = jedis.get("url#" + key);
+        if (redirectUrl == null) {
+            notFound();
         }
+        redirect(redirectUrl);
     }
 
     public static String postUrl(String url) {
-        Jedis jedis = null;
-        try {
-            jedis = pool.getResource();
-            String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-            int size = 1;
-            String key = null, exitingUrl = null;
-            do {
-                key = RandomStringUtils.random(size, letters);
-                exitingUrl = findUrl(key);
-                size++;
-            } while (exitingUrl != null);
+        Jedis jedis = new Jedis(redisConfig);
+        String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+        int size = 1;
+        String key = null, exitingUrl = null;
+        do {
+            key = RandomStringUtils.random(size, letters);
+            exitingUrl = findUrl(key);
+            size++;
+        } while (exitingUrl != null);
 
-            String niceUrl = url.startsWith("http://") || url.startsWith("https://") ? url : "http://" + url;
-            jedis.set("url#" + key, niceUrl);
-            return key;
-        } finally {
-            if (jedis != null) {
-                pool.returnResource(jedis);
-            }
-        }
+        String niceUrl = url.startsWith("http://") || url.startsWith("https://") ? url : "http://" + url;
+        jedis.set("url#" + key, niceUrl);
+        return key;
     }
 
     private static String findUrl(String key) {
-        Jedis jedis = null;
-        try {
-            jedis = pool.getResource();
-            return jedis.get("url#" + key);
-        } finally {
-            if (jedis != null) {
-                pool.returnResource(jedis);
-            }
-        }
+        Jedis jedis = new Jedis(redisConfig);
+        return jedis.get("url#" + key);
     }
 }
